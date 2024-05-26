22f05a2ace6131da47fb6bb8b0e8ffa7aec3d235
Richard <123456789+macabusrichard@gmail.com>
2024-05-26 12:57:50 +0800

    fixed all the function of width and height of the div

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 const container = document.querySelector('.container');
 
-for(let i = 1; i <= 16; i++){
-  const gridVertical = document.createElement('div');
-  gridVertical.style.cssText = "display: flex; justify-content: center;";
-  container.appendChild(gridVertical);
-  
-  for(let i = 1; i <= 16; i++){
-    const gridHorizontal = document.createElement('div');
-    gridHorizontal.classList.add('horizontal');
-    gridVertical.appendChild(gridHorizontal);
-  }
-} 
 
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', () => {
-  let choice = prompt("how many px per side do you want to see? (max 100px)");
-  container.innerHTML = "";
-  
-  perSide(choice);
-});
-
-function perSide(n) {
+function squareSize(n) {
   if (n > 100) {
     n = 100;
   }else if (n === null || n === "") {
     n = 16;
   }
-  for(let i = 1; i <= n; i++) {
+  
+  for(let i = 1; i <= n; i++){
+    
+    
     const gridVertical = document.createElement('div');
+    gridVertical.classList.add('vertical');
     gridVertical.style.cssText = "display: flex; justify-content: center;";
     container.appendChild(gridVertical);
     
     for(let i = 1; i <= n; i++){
+      const size = (600 - (n * 2)) / n;
       const gridHorizontal = document.createElement('div');
       gridHorizontal.classList.add('horizontal');
-      // gridHorizontal.textContent = `${i}`;
+      gridHorizontal.style.width = `${size}px`;
+      gridHorizontal.style.height = `${size}px`;
+      gridHorizontal.style.justifyContent = "center";
       gridVertical.appendChild(gridHorizontal);
     }
-  }
+  } 
 }
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  let choice = prompt("how many px per side? (max 100px)");
+  container.innerHTML = "";
+  
+  squareSize(choice);
+});
+
+squareSize(16);
