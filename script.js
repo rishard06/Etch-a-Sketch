c506b9f001accd9160fe13854860c486c78c0746
Richard <123456789+macabusrichard@gmail.com>
2024-05-28 11:35:44 +0800

    fix the clear button

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const clearBG = document.querySelector('.clear');
 clearBG.addEventListener('click', () => {
   horizontal.forEach(horizon =>  {
     horizon.style.backgroundColor = "white";
+    horizon.style.opacity = "";
   });
 });
 
